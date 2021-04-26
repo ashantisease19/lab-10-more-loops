8b1745082ff162422b159eb718406ed20f879778
Ashanti Sease <you@example.com>
2021-04-26 16:22:42 -0400

    finished in class

---
+++ gimmemynumber.py
@@ -0,0 +1,9 @@
+userstring = input("Gimme a number bro that is greater that 100.")
+usernum = int(userstring)
+
+while usernum < 100:
+    print(str(usernum) + " is less than 100. You're not dumb.")
+    userstring = print(str(usernum) + " is less than 100. You're not dumb.")
+    usernum = int(userstring)
+
+print("Oh, snap " + str(usernum) + " is greater. You did it, bro.")
